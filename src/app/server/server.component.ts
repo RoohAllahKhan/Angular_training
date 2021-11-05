f4f32e521e8e551ee3c0fb4dd7085af29d8d527e
RoohAllahKhan <roohallahkhan20@gmail.com>
2021-11-05 18:10:33 +0500

    Project.

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+//JS object in this component to configure it otherwise it's not valuable to Angular
+//Stores some information which will be stored as metadata for this class which will tell Angular what to do with this class
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
