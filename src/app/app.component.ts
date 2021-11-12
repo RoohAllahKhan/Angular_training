915484a15cc4b32a805c1b2dd17c511e88a084ca
RoohAllahKhan <roohallahkhan20@gmail.com>
2021-11-12 19:43:03 +0500

    Custom structural Directive.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  serverFlag: boolean = false;
   serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
 
   onServerAdded(serverData: {serverName: string, serverContent: string}) {
